e5e5069a863ebc64e98e8480d3e382e7ca925bad
dev2_team02_dogs_danny <tjkueon0309@gmail.com>
2026-01-19 16:21:05 +0900

    [Fix] #125 - seller 모듈 kafkaTemplate 충돌 문제 해결

--- baro-seller/src/main/java/com/barofarm/seller/farm/infrastructure/kafka/KafkaProducerConfig.java
+++ baro-seller/src/main/java/com/barofarm/seller/farm/infrastructure/kafka/KafkaProducerConfig.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -43,6 +44,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
+    @Primary
     public KafkaTemplate<String, FarmEvent> farmEventKafkaTemplate() {
         return new KafkaTemplate<>(farmEventProducerFactory());
     }
